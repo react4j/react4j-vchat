d0f6e0fa5d91d91d68f117a3af948e39404d687a
Peter Donald <peter@realityforge.org>
2020-07-25 12:22:12 +1000

    Simplify further

--- src/main/java/react4j/vchat/BrowserLocation.java
+++ src/main/java/react4j/vchat/BrowserLocation.java
@@ -5,15 +5,12 @@ import arez.annotations.Action;
 import arez.annotations.ArezComponent;
 import arez.annotations.ComputableValueRef;
 import arez.annotations.DepType;
-import arez.annotations.Feature;
 import arez.annotations.Memoize;
-import arez.annotations.Observable;
 import arez.annotations.OnActivate;
 import arez.annotations.OnDeactivate;
 import elemental2.dom.DomGlobal;
 import elemental2.dom.Event;
 import elemental2.dom.EventListener;
-import java.util.Objects;
 import javax.annotation.Nonnull;
 
 /**
@@ -22,21 +19,11 @@ import javax.annotation.Nonnull;
  *
  * @link https://github.com/arez/arez-browserlocation
  */
-@ArezComponent( service = Feature.ENABLE, requireId = Feature.DISABLE )
+@ArezComponent
 abstract class BrowserLocation
 {
   @Nonnull
   private final EventListener _listener = this::onHashChangeEvent;
-  /**
-   * The location according to the application.
-   */
-  @Nonnull
-  private String _location;
-  /**
-   * The location that the application is attempting to update the browser to.
-   */
-  @Nonnull
-  private String _targetLocation;
 
   /**
    * Create the model object.
@@ -49,66 +36,12 @@ abstract class BrowserLocation
     return new Arez_BrowserLocation();
   }
 
-  BrowserLocation()
-  {
-    _targetLocation = _location = getHash();
-  }
-
-  /**
-   * Change the target location to the specified parameter.
-   * This will ultimately result in a side-effect that updates the browsers location.
-   * This location parameter should not include "#" as the first character.
-   *
-   * @param targetLocation the location to change to.
-   */
-  @Action( verifyRequired = false )
-  void changeLocation( @Nonnull final String targetLocation )
-  {
-    _targetLocation = targetLocation;
-    if ( targetLocation.equals( getBrowserLocation() ) )
-    {
-      setLocation( targetLocation );
-    }
-    setHash( targetLocation );
-    /*
-     * setHash does not trigger a "hashchange" event so explicitly call the hook here
-     */
-    updateBrowserLocation();
-  }
-
-  /**
-   * Revert the browsers location to the application location.
-   */
-  @Action
-  void resetBrowserLocation()
-  {
-    changeLocation( getLocation() );
-  }
-
-  /**
-   * Return the location as the application sees it.
-   * This return value does not include a "#" as the first character.
-   *
-   * @return the location.
-   */
-  @Observable
-  @Nonnull
-  String getLocation()
-  {
-    return _location;
-  }
-
-  @Observable
-  void setLocation( @Nonnull final String location )
-  {
-    _location = Objects.requireNonNull( location );
-  }
-
   @Memoize( depType = DepType.AREZ_OR_EXTERNAL )
   @Nonnull
   String getBrowserLocation()
   {
-    return getHash();
+    final String hash = DomGlobal.window.location.hash;
+    return null == hash ? "" : hash.substring( 1 );
   }
 
   @OnActivate
@@ -130,40 +63,11 @@ abstract class BrowserLocation
   void updateBrowserLocation()
   {
     getBrowserLocationComputableValue().reportPossiblyChanged();
-    final String location = getBrowserLocation();
-    if ( _targetLocation.equals( location ) )
-    {
-      setLocation( location );
-    }
   }
 
   private void onHashChangeEvent( @Nonnull final Event e )
   {
     e.preventDefault();
     updateBrowserLocation();
-  }
-
-  @Nonnull
-  private String getHash()
-  {
-    final String hash = DomGlobal.window.location.hash;
-    return null == hash ? "" : hash.substring( 1 );
-  }
-
-  private void setHash( @Nonnull final String hash )
-  {
-    if ( 0 == hash.length() )
-    {
-      /*
-       * This code is needed to remove the stray #.
-       * See https://stackoverflow.com/questions/1397329/how-to-remove-the-hash-from-window-location-url-with-javascript-without-page-r/5298684#5298684
-       */
-      final String url = DomGlobal.window.location.pathname + DomGlobal.window.location.search;
-      DomGlobal.window.history.pushState( "", DomGlobal.document.title, url );
-    }
-    else
-    {
-      DomGlobal.window.location.hash = hash;
-    }
   }
 }
