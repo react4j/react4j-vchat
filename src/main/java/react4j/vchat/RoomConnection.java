58b2e0db8195ed0478b0c7485459e87c5ed1b839
Peter Donald <peter@realityforge.org>
2020-07-26 08:56:11 +1000

    Start to add state for local video tracking

--- src/main/java/react4j/vchat/RoomConnection.java
+++ src/main/java/react4j/vchat/RoomConnection.java
@@ -3,7 +3,10 @@ package react4j.vchat;
 import arez.annotations.Action;
 import arez.annotations.ArezComponent;
 import arez.annotations.ComponentDependency;
+import arez.annotations.Feature;
 import arez.annotations.Observable;
+import elemental3.DOMException;
+import elemental3.MediaStream;
 import java.util.Objects;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -17,7 +20,7 @@ abstract class RoomConnection
   @Nonnull
   static RoomConnection create( @Nonnull final BrowserLocation browserLocation )
   {
-    return new Arez_RoomConnection( browserLocation );
+    return new Arez_RoomConnection( browserLocation, true, true );
   }
 
   RoomConnection( @Nonnull final BrowserLocation browserLocation )
@@ -32,7 +35,7 @@ abstract class RoomConnection
     return location.isEmpty() ? null : location;
   }
 
-  @Observable
+  @Observable( initializer = Feature.ENABLE )
   abstract boolean isAudioEnabled();
 
   abstract void setAudioEnabled( boolean audioEnabled );
@@ -41,9 +44,15 @@ abstract class RoomConnection
   void toggleAudio()
   {
     setAudioEnabled( !isAudioEnabled() );
+    final MediaStream localStream = getLocalStream();
+    if ( null != localStream )
+    {
+
+      localStream.getAudioTracks().forEach( ( track, index, array ) -> track.enabled = !track.enabled );
+    }
   }
 
-  @Observable
+  @Observable( initializer = Feature.ENABLE )
   abstract boolean isVideoEnabled();
 
   abstract void setVideoEnabled( boolean videoEnabled );
@@ -52,6 +61,11 @@ abstract class RoomConnection
   void toggleVideo()
   {
     setVideoEnabled( !isVideoEnabled() );
+    final MediaStream localStream = getLocalStream();
+    if ( null != localStream )
+    {
+      localStream.getVideoTracks().forEach( ( track, index, array ) -> track.enabled = !track.enabled );
+    }
   }
 
   boolean isConnecting()
@@ -62,5 +76,52 @@ abstract class RoomConnection
   boolean isWaiting()
   {
     return true;
+  }
+
+  @Observable( writeOutsideTransaction = Feature.ENABLE )
+  @Nullable
+  abstract MediaStream getLocalStream();
+
+  abstract void setLocalStream( @Nullable MediaStream localStream );
+
+  @Observable( writeOutsideTransaction = Feature.ENABLE )
+  @Nullable
+  abstract String getLocalStreamErrorMessage();
+
+  abstract void setLocalStreamErrorMessage( @Nullable String message );
+
+  void onLocalStream( @Nonnull final MediaStream stream )
+  {
+    setLocalStream( stream );
+    setLocalStreamErrorMessage( null );
+  }
+
+  void onLocalStreamError( @Nonnull final DOMException exception )
+  {
+    setLocalStream( null );
+    switch ( exception.name() )
+    {
+      case "NotAllowedError":
+        setLocalStreamErrorMessage( "The user did not all allow access to the audio or video stream" );
+        break;
+      case "NotFoundError":
+        setLocalStreamErrorMessage( "The computing device does not support devices to capture " +
+                                    "audio and/or video streams" );
+        break;
+      case "NotReadableError":
+        setLocalStreamErrorMessage( "A hardware error occurred trying to retrieve data from " +
+                                    "the audio and/or video devices" );
+        break;
+      case "SecurityError":
+        setLocalStreamErrorMessage( "The security policy has blocked access to the audio or video stream" );
+        break;
+      case "AbortError":
+        setLocalStreamErrorMessage( "Unexpected error occurred accessing the audio or video stream: " +
+                                    exception.message() );
+        break;
+      default:
+        setLocalStreamErrorMessage( "Unexpected error occurred accessing the audio or video stream" );
+        break;
+    }
   }
 }
