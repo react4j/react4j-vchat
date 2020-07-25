004a1c96f196931fa1fd810f5bb0fc2aa36f7ddc
Peter Donald <peter@realityforge.org>
2020-07-25 13:40:53 +1000

    Hook up components in first pass

--- src/main/java/react4j/vchat/Application.java
+++ src/main/java/react4j/vchat/Application.java
@@ -1,18 +1,29 @@
 package react4j.vchat;
 
+import arez.annotations.CascadeDispose;
 import javax.annotation.Nonnull;
 import react4j.ReactNode;
 import react4j.annotations.Render;
 import react4j.annotations.View;
 import static react4j.dom.DOM.*;
 
-@View
+@View( type = View.Type.TRACKING )
 abstract class Application
 {
+  @CascadeDispose
+  @Nonnull
+  final BrowserLocation _location = BrowserLocation.create();
+  @CascadeDispose
+  @Nonnull
+  final RoomConnection _connection = RoomConnection.create( _location );
+
   @Nonnull
   @Render
   ReactNode render()
   {
-    return h1( "Video Chat" );
+    final String location = _location.getLocation();
+    return location.isEmpty() ?
+           SelectRoomViewBuilder.build() :
+           h1( "Room " + _connection.getRoomId() );
   }
 }
