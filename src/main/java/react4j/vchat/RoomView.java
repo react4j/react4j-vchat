16fb6496c275ff16fe060720e1d8f1af0ed03d18
Peter Donald <peter@realityforge.org>
2020-07-26 21:28:12 +1000

    Add some placeholdre code to show local videos

--- src/main/java/react4j/vchat/RoomView.java
+++ src/main/java/react4j/vchat/RoomView.java
@@ -7,9 +7,9 @@ import jsinterop.base.JsPropertyMap;
 import react4j.ReactElement;
 import react4j.ReactNode;
 import react4j.annotations.Input;
+import react4j.annotations.PostMount;
 import react4j.annotations.Render;
 import react4j.annotations.View;
-import react4j.dom.events.MouseEvent;
 import react4j.dom.proptypes.html.BtnProps;
 import react4j.dom.proptypes.html.HtmlProps;
 import react4j.dom.proptypes.html.ImgProps;
@@ -20,80 +20,88 @@ import static react4j.dom.DOM.*;
 abstract class RoomView
 {
   @Nullable
-  private HTMLVideoElement _localVideo;
-  @Nullable
   private HTMLVideoElement _remoteVideo;
 
   @Input( immutable = true )
   @Nonnull
   abstract RoomConnection connection();
 
+  @PostMount
+  void postMount()
+  {
+    connection().connectLocalMedia();
+  }
+
   @Nonnull
   @Render
   ReactNode render()
   {
     final RoomConnection connection = connection();
-    final RefConsumer localRef = e -> _localVideo = (HTMLVideoElement) e;
+    final RefConsumer localRef = e -> connection().setLocalVideoElement( (HTMLVideoElement) e );
+    final RefConsumer screenShareRef = e -> connection().setScreenShareVideoElement( (HTMLVideoElement) e );
     final RefConsumer removeRef = e -> _remoteVideo = (HTMLVideoElement) e;
     return div( new HtmlProps().className( "video-wrapper" ),
                 div( new HtmlProps().className( "local-video-wrapper" ),
                      ReactElement.createHostElement( "video",
                                                      null,
                                                      localRef,
-                                                     JsPropertyMap.of( "autoPlay", true,
-                                                                       "muted", "",
-                                                                       "id", "localVideo" ) ),
-                     ReactElement.createHostElement( "video",
-                                                     null,
-                                                     removeRef,
                                                      JsPropertyMap.of( "autoPlay",
                                                                        true,
-                                                                       "className",
-                                                                       connection.isConnecting() ||
-                                                                       connection.isWaiting() ? "hide" : "",
+                                                                       "muted",
+                                                                       true,
                                                                        "id",
-                                                                       "remoteVideo" ) ),
-                     div( new HtmlProps().className( "controls" ),
-                          button( new BtnProps().className( "control-btn" ).onClick( this::shareScreen ),
-                                  // TODO: Should generate svg factory methods and props so don't have to ref as img
-                                  img( new ImgProps().src( "img/shared_screen.svg" ).width( 32 ).height( 32 ) )
-                          ),
-                          button( new BtnProps().className( "control-btn" ).onClick( this::toggleAudio ),
-                                  // TODO: Should generate svg factory methods and props so don't have to ref as img
-                                  img( new ImgProps()
-                                         .src( connection.isAudioEnabled() ? "img/mic_on.svg" : "img/mic_off.svg" )
-                                         .width( 32 )
-                                         .height( 32 ) )
-                          ),
-                          button( new BtnProps().className( "control-btn" ).onClick( this::toggleVideo ),
-                                  // TODO: Should generate svg factory methods and props so don't have to ref as img
-                                  img( new ImgProps()
-                                         .src( connection.isVideoEnabled() ? "img/cam_on.svg" : "img/cam_off.svg" )
-                                         .width( 32 )
-                                         .height( 32 ) )
-                          )
+                                                                       "localVideo" ) ),
+                     ReactElement.createHostElement( "video",
+                                                     null,
+                                                     screenShareRef,
+                                                     JsPropertyMap.of( "autoPlay",
+                                                                       true,
+                                                                       "muted",
+                                                                       true,
+                                                                       "id",
+                                                                       "screenShareVideo" ) )
+                ),
+                ReactElement.createHostElement( "video",
+                                                null,
+                                                removeRef,
+                                                JsPropertyMap.of( "autoPlay",
+                                                                  true,
+                                                                  "className",
+                                                                  connection.isConnecting() ||
+                                                                  connection.isWaiting() ? "hide" : "",
+                                                                  "id",
+                                                                  "remoteVideo" ) ),
+                div( new HtmlProps().className( "controls" ),
+                     button( new BtnProps().className( "control-btn" ).onClick( e -> connection.toggleScreenShare() ),
+                             // TODO: Should generate svg factory methods and props so don't have to ref as img
+                             img( new ImgProps()
+                                    .src( connection.isScreenShareEnabled() ?
+                                          "img/screen_share_on.svg" :
+                                          "img/screen_share_off.svg" )
+                                    .width( 32 )
+                                    .height( 32 ) )
                      ),
-                     connection.isConnecting() ?
-                     div( new HtmlProps().className( "status" ), p( "Establishing connection..." ) ) :
-                     null,
-                     connection.isWaiting() ?
-                     div( new HtmlProps().className( "status" ), p( "Waiting for someone..." ) ) :
-                     null
-                )
+                     button( new BtnProps().className( "control-btn" ).onClick( e -> connection.toggleAudio() ),
+                             // TODO: Should generate svg factory methods and props so don't have to ref as img
+                             img( new ImgProps()
+                                    .src( connection.isAudioEnabled() ? "img/mic_on.svg" : "img/mic_off.svg" )
+                                    .width( 32 )
+                                    .height( 32 ) )
+                     ),
+                     button( new BtnProps().className( "control-btn" ).onClick( e -> connection.toggleVideo() ),
+                             // TODO: Should generate svg factory methods and props so don't have to ref as img
+                             img( new ImgProps()
+                                    .src( connection.isVideoEnabled() ? "img/cam_on.svg" : "img/cam_off.svg" )
+                                    .width( 32 )
+                                    .height( 32 ) )
+                     )
+                ),
+                connection.isConnecting() ?
+                div( new HtmlProps().className( "status" ), p( "Establishing connection..." ) ) :
+                null,
+                connection.isWaiting() ?
+                div( new HtmlProps().className( "status" ), p( "Waiting for someone..." ) ) :
+                null
     );
-  }
-
-  private void toggleVideo( @Nonnull final MouseEvent event )
-  {
-    connection().toggleVideo();
-  }
-
-  private void toggleAudio( @Nonnull final MouseEvent event )
-  {
-    connection().toggleAudio();
-  }
-
-  private void shareScreen( @Nonnull final MouseEvent event )
-  {
   }
 }
