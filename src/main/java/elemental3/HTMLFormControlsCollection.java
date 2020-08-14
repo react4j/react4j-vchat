d38458e67757d4169e57e8f3aada01eadf4560bb
Peter Donald <peter@realityforge.org>
2020-08-15 08:24:52 +1000

    Regenerate

--- src/main/java/elemental3/HTMLFormControlsCollection.java
+++ src/main/java/elemental3/HTMLFormControlsCollection.java
@@ -1,7 +1,10 @@
 package elemental3;
 
 import javax.annotation.Generated;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 /**
@@ -15,7 +18,28 @@ import jsinterop.annotations.JsType;
     namespace = JsPackage.GLOBAL,
     name = "HTMLFormControlsCollection"
 )
-public class HTMLFormControlsCollection extends HTMLCollection {
+public class HTMLFormControlsCollection {
   HTMLFormControlsCollection() {
   }
+
+  @JsProperty(
+      name = "length"
+  )
+  public native int length();
+
+  /**
+   * The HTMLCollection method item() returns the node located at the specified offset into the collection.
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection/item">HTMLCollection.item - MDN</a>
+   */
+  @Nullable
+  public native RadioNodeListOrElementUnion item(int index);
+
+  /**
+   * The HTMLFormControlsCollection.namedItem() method returns the RadioNodeList or the Element in the collection whose name or id match the specified name, or null if no node matches.
+   *
+   * @see <a href="https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormControlsCollection/namedItem">HTMLFormControlsCollection.namedItem - MDN</a>
+   */
+  @Nullable
+  public native RadioNodeListOrElementUnion namedItem(@Nonnull String name);
 }
