cd32b2fb57c787f2db6d8fed83b365ea79934b1c
Peter Donald <peter@realityforge.org>
2020-08-15 09:07:30 +1000

    Add missing import

--- src/main/java/react4j/vchat/SelectRoomView.java
+++ src/main/java/react4j/vchat/SelectRoomView.java
@@ -8,6 +8,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import jsinterop.base.Js;
 import react4j.ReactNode;
+import react4j.annotations.Input;
 import react4j.annotations.Render;
 import react4j.annotations.View;
 import react4j.dom.events.FormEvent;
