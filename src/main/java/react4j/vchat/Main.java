3c5e439fb6e14f6bb3655b8b78214a30837652e4
Peter Donald <peter@realityforge.org>
2021-04-29 15:04:39 +1000

    Remove Elemental2 support code

--- src/main/java/react4j/vchat/Main.java
+++ src/main/java/react4j/vchat/Main.java
@@ -3,7 +3,6 @@ package react4j.vchat;
 import akasha.Element;
 import akasha.WindowGlobal;
 import com.google.gwt.core.client.EntryPoint;
-import jsinterop.base.Js;
 import react4j.dom.ReactDOM;
 import react4j.vchat.view.ApplicationBuilder;
 
@@ -16,7 +15,6 @@ public final class Main
     //ReactArezSpyUtil.enableSpyEventLogging();
     final Element root = WindowGlobal.document().getElementById( "app" );
     assert null != root;
-    // TODO: Remove Js.cast once react4j is moved to elemental3
-    ReactDOM.render( ApplicationBuilder.build(), Js.cast( root ) );
+    ReactDOM.render( ApplicationBuilder.build(), root );
   }
 }
